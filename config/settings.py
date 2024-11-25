72310321f0c13cb841af6a881f9a3619b466e95b
Sean McHale <seanryanmchale@yahoo.com>
2024-11-25 15:57:28 +0800

    deploying debugging

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,10 @@ DEFAULT_API_VERSION = os.getenv('DEFAULT_API_VERSION', 'v1')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if ENVIRONMENT == 'development' else False
+# DEBUG = True if ENVIRONMENT == 'development' else False
+DEBUG = True
 
-ALLOWED_HOSTS = ['huispeak-7ace14a2aaa6.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
